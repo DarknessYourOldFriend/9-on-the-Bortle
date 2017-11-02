0e787df232b458572dc253c43cd5379f9b8e6d0d
Unknown <okh216@nyu.edu>
2017-11-02 16:37:34 -0400

    Unity Files
    Added all the unity files for the project, general idea mapped out. Need to add space bar functionality and collider interactions

---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+    //public float playerSpeed = 1.0f; //Speed of player input
+    public float horizonSpeed = 1.0f; //Speed of horizontal auto movement
+    public float warpUp = 3.0f; //Position of player changes without physics
+    public float warpDown = - 3.0f;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector3(horizonSpeed, 0, 0); //Player is contantly moving horizontally
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //rb.velocity = new Vector3(horizonSpeed, +playerSpeed, 0);
+            //Movement without Physics
+     		Vector3 position = transform.position;
+            
+            position.y += warpUp;
+            transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //rb.velocity = new Vector3(horizonSpeed, -playerSpeed, 0);
+
+            //Movement without Physics
+            Vector3 position = transform.position;
+            position.y -= warpDown;
+            transform.position = position;
+        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+
+        //}
+        
+
+
+    }
+}
