d60fc04dff5d989530fc5a5491d53dfcb8435b3f
Unknown <okh216@nyu.edu>
2017-11-06 23:15:33 -0500

    Initial Files
    Inital files from the dropbox, the player movement script now has a limiter to keep the player from going off the screen

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public Transform followObject; //Slide object into this slot to use their transform
+    public float offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 currentPosition = transform.position;
+
+        //Mathf.Lerp is a function for blending two values some amount
+        //if I give it the parameters (a,b,0.5f) then it will return a 50% blend of a and b
+        //if I give it the parameter (a,b,0.1f) then it will be 10% a and 90% b
+        //here we use it to make the camera go 50% of the way to the followObject's position every frame.
+        currentPosition.x = Mathf.Lerp(currentPosition.x, followObject.position.x + offset, 0.25f);
+
+        //now that we've modified the position, we can copy the value back to the transform
+        transform.position = currentPosition;
+
+    }
+}
---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+    //public float playerSpeed = 1.0f; //Speed of player input
+    public float horizonSpeed = 1.0f; //Speed of horizontal auto movement
+    public float warpUp = 3.0f; //Position of player changes without physics
+    public float warpDown = -3.0f;
+    private float playerPosition;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        playerPosition = GameObject.Find("Player").transform.position.y;
+        rb.velocity = new Vector3(horizonSpeed, 0, 0); //Player is contantly moving horizontally
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (playerPosition != 3)) //!= 3 keeps player from moving too far up (can't believe I didn't think of that sooner)  
+        {
+            //rb.velocity = new Vector3(horizonSpeed, +playerSpeed, 0);
+            //Movement without Physics
+     		Vector3 position = transform.position;
+            
+            position.y += warpUp;
+            transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && (playerPosition != -3)) //&& (playerPosition != 0))
+        {
+            //rb.velocity = new Vector3(horizonSpeed, -playerSpeed, 0);
+
+            //Movement without Physics
+            Vector3 position = transform.position;
+            position.y -= warpDown;
+            transform.position = position;
+        }
+        //if (Input.GetKey(KeyCode.Space) && (thisCollision.collider.tag == "Destroy"))
+        //{
+        //Debug.Log("hit " + thisCollision.collider.name);
+        //Destroy(thisCollision.gameObject);
+        //}
+
+
+
+    }
+}
