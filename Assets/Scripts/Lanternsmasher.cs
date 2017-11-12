efbc2a7ac4e24f8d601e067c35242bc57f83e234
Unknown <okh216@nyu.edu>
2017-11-12 17:26:48 -0500

    Added Comments to code
    Prepping for project sharing

--- Assets/Scripts/Lanternsmasher.cs
+++ Assets/Scripts/Lanternsmasher.cs
@@ -16,13 +16,13 @@ public class Lanternsmasher : MonoBehaviour
     }
     void update()
     {
-        if ((overEnemy == true) && (Input.GetKeyDown(KeyCode.Space)))
+        if ((overEnemy == true) && (Input.GetKeyDown(KeyCode.Space))) //Tells the square under player to be destroyed
             Destroy(thisCollision.gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D thisCollision)
     {
-        if (thisCollision.GetComponent<Collider2D>().tag == "Enemy")
+        if (thisCollision.GetComponent<Collider2D>().tag == "Enemy") //Grabs the name of object, says player is going ove it
         {
             Debug.Log("The Smasher hit " + thisCollision.name);
             collidedWith = thisCollision.gameObject;
@@ -31,7 +31,7 @@ public class Lanternsmasher : MonoBehaviour
     }
     void OnTriggerExit2D(Collider2D thisCollision)
     {
-        if (thisCollision.GetComponent<Collider2D>().tag == "Enemy")
+        if (thisCollision.GetComponent<Collider2D>().tag == "Enemy") //Switches trigger to go back to false
         {
             Debug.Log("It works, just exited:" + thisCollision.name);
             overEnemy = false;
--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -38,6 +38,7 @@ public class Playermovement : MonoBehaviour
         {
             //rb.velocity = new Vector3(horizonSpeed, +playerSpeed, 0);
             //Movement without Physics
+            // uses Simplelanechanger script, but horizontal instead of vertical
             Vector3 position = transform.position;
 
             position.y += warpUp;
@@ -52,13 +53,13 @@ public class Playermovement : MonoBehaviour
             position.y -= warpDown;
             transform.position = position;
         }
-        if ((Input.GetKeyDown(KeyCode.Space) && (smasher.overEnemy == true)))
+        if ((Input.GetKeyDown(KeyCode.Space) && (smasher.overEnemy == true))) //Pressing space when player is over a lantern destroys it
         {
             Debug.Log("It works, just popped:" + smasher.collidedWith.gameObject);
             Destroy(smasher.collidedWith.gameObject);
             //lighter.playerPressed = true;
         }
-        if ((Input.GetKeyDown(KeyCode.Space)) && (gamestart.preGamestate == true))
+        if ((Input.GetKeyDown(KeyCode.Space)) && (gamestart.preGamestate == true)) //Tried to make game start screen same scene as main game scene Made a new scene instead.
         {
             gamestart.preGamestate = false;
         }
