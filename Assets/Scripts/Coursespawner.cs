2f7126155254d20338a176925fc318b685718fd0
Unknown <okh216@nyu.edu>
2017-11-12 23:16:51 -0500

    Course Spawner Testing
    Creating course with coursespawner right now, going to test timing in GL5 build.

--- Assets/Scripts/Coursespawner.cs
+++ Assets/Scripts/Coursespawner.cs
@@ -20,7 +20,7 @@ public class Coursespawner : MonoBehaviour {
         if ((Input.GetKeyDown(KeyCode.Space)) && (smasher.overEnemy == false))
         { //if the spacebar is pressed, and player not over a lantern
             GameObject lantern = Instantiate(lanternPrefab);   //Mafe a gameObject from the prefab, store a reference to it in the newBullet var
-            lantern.transform.position = new Vector2(transform.position.x, transform.position.y);
+            lantern.transform.position = new Vector2(transform.position.x + 1, transform.position.y);
 
             Rigidbody2D rb = lantern.GetComponent<Rigidbody2D>(); //set rb to a reference to the Rigidbody2D on the newBullet gameObject
         }
